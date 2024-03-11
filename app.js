b1a0e40e73ac888b4ac394c9db6cc53922061687
mucz <zagorskitobiasz@gmail.com>
2024-03-11 12:28:40 +0100

    Stan na 12.30 dodanie kilku zmienych oraz pousuwanie nie potrzebnych komentarzy

--- app.js
+++ app.js
@@ -10,46 +10,24 @@ let start = document.querySelector(".start");
 let board = document.querySelector(".board");
 let reset = document.querySelector(".reset");
 let stop = document.querySelector(".stop");
-
-//tab[4][4] = div
-
-
-//Create div
-/*let divArray = []
-
-
-for () {
-  for () {
-    let div = createDiv();
-    tab[y][x] = div;
-  }
-}
-
-function setPos() {
-
-  for () {
-    for () {
-      let div = tab[y][x];
-      div.left =
-        div.top =
-    }
-  }
-}
-*/
+let divBoard = [];
+let Interval = "";
 
 function startInt() {
-  setInterval(pressStart, 1000)
-}
-/*
-function stopInt(){
-  clearInterval()
+  Interval= setInterval(pressStart, 1000)
 }
 
- */
+function stopInt(){
+   clearInterval(Interval)
+}
+
+
+
 
 //Tablica z Div
 function pressStart(){
-  let divBoard = [];
+
+  board.innerHTML = '';
   for (let i = 0; i < rows; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -62,19 +40,35 @@ function pressStart(){
       div.style.backgroundColor = Math.random() < 0.7 ? 'white': 'black';  //czarna żywa
       div.style.margin = '2px';
       row.appendChild(div);
-     /* if (div.style.backgroundColor === 'black') {
-        divBoard[0][0] = div.style.backgroundColor === "blue";
-      }*/
+      board.appendChild(row);
+      divBoard.push(row);
     }
     board.appendChild(row);
-    divBoard.push(row);
+
 
   }
 
   console.log(divBoard);
-
+  sprawdzenieSasiadow()
 }
 
+
+function sprawdzenieSasiadow() {
+  let tablicaSasiadow = [];
+
+  for (let i = 0; i < divBoard.length; i++) {
+
+  }
+
+
+  console.log(tablicaSasiadow);
+}
+
+
 start.addEventListener('click',pressStart);
-//start.addEventListener('click',startInt);
-//stop.addEventListener('click',stopInt)
+//start.addEventListener('click',sprawdzenieSasiadow);
+start.addEventListener('click',startInt);
+stop.addEventListener('click',stopInt)
+
+
+
