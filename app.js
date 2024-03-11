210c05a98049acd8b0aece889563702bd3e66b4a
unknown <zagorskitobiasz@gmail.com>
2024-03-11 18:57:39 +0100

    Dodano poprawe błedów oraz wporwadzon fukcje sprawdzania sasiadow 19.00 11.03.2024

--- app.js
+++ app.js
@@ -1,111 +1,96 @@
 //debugger;
 
-
-
-let writing = '';
 /*let row = prompt("Podaj ilość wierszy");
 let column = prompt("Podaj ilość column");*/
 
 let rows =10;
-let column =10;
+let columns =10;
 
+let divy = document.querySelector(".class")
 let start = document.querySelector(".start");
 let board = document.querySelector(".board");
 let reset = document.querySelector(".reset");
 let stop = document.querySelector(".stop");
-
-//tab[4][4] = div
-
-//Konsola
-/*let tab = [
-    [0,0,0,0,0,0,0,1,0,0],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,9,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10],
-    [1,2,3,4,5,6,7,8,9,10]
-];
-console.log(tab);*/
-
-//Create div
-/*let divArray = []
-
-
-for () {
-  for () {
-    let div = createDiv();
-    tab[y][x] = div;
-  }
+let divBoard = [];
+let Interval = "";
+/*
+function startInt() {
+  Interval= setInterval(To jest zlepressStart, 1000)
 }
 
-function setPos() {
-
-  for () {
-    for () {
-      let div = tab[y][x];
-      div.left =
-        div.top =
-    }
-  }
+function stopInt(){
+   clearInterval(Interval)
+}
+function resetBoard(){
+  board.innerHTML = '';
 }
 */
 
 
-//Tablica z Div
-function pressStart(){
-  let divBoard = [];
 
+//Tablica z Div 
+function pressStart(){
+
+  board.innerHTML = '';
   for (let i = 0; i < rows; i++) {
-    let row = document.createElement('div'); 
+    let row = document.createElement('div');
     row.classList.add('row');
-    for (let j = 0; j < column; j++) {
+    for (let j = 0; j < columns; j++) {
       let div = document.createElement('div');
       div.classList.add('class');
       div.style.border = '1px solid black';
       div.style.width = '20px';
       div.style.height = '20px';
-      div.style.backgroundColor = Math.random() < 0.7 ? 'white': 'black';
-      div.style.margin = '2px'; 
-      row.appendChild(div); 
+      div.style.backgroundColor = Math.random() < 0.7 ? 0: "black";  //czarna żywa
+      div.style.margin = '2px';
+      row.appendChild(div);
+      board.appendChild(row);
+      divBoard.push(row);
     }
-    board.appendChild(row); 
-    divBoard.push(row); 
+    board.appendChild(row);
+
+
   }
 
   console.log(divBoard);
 }
 
 
-/*
-  function createDiv() {
-    for (let i = 0; i < 30; i++) {
-      let div = document.createElement('div');
-      let divArray = [[]];
-      divArray.push(div);
-      div.innerHTML = "div";
-      div.classList.add('class');
-      div.style.border = '1px solid black';
-      div.style.width = '20px';
-      div.style.height = '20px';
-      div.style.backgroundColor = 'black';
-      board.appendChild(div);
-
-    }
-  }
-
-//Create board
-  function pressStart(divArray) {
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < column; j++) {
+//Sprawdzenie sąsiadów 
+function countNeighbors(x,y) {
+  let board = [];
+  let count =0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      const neighborX = x + i;
+      const neighborY = y + j;
+      if (i === 0 && j === 0) continue; //Dzieki niej sprawdzamy sasiadow danej komorki
+      if (neighborX >= 0 && neighborX < rows && neighborY >= 0 && neighborY < columns) {
+        count += board[neighborX][neighborY] ? 1 : 0;
       }
-      writing += divArray + '<br>';
-
     }
-    document.querySelector(".board").innerHTML = writing;
   }
-*/
+  console.log("Liczba sąsiadów (" + x + ", " + y + "): " + count);
+}
+
+
+//Aktualizuje plasze
+function updateBoardState() {
+
+}
+
+//Rozpoczyna gre
+function startGame() {
+
+}
+
+
 start.addEventListener('click',pressStart);
+start.addEventListener('click',countNeighbors);
+//start.addEventListener('click',startInt);
+//stop.addEventListener('click',stopInt);
+//reset.addEventListener('click',resetBoard);
+
+
+
+
